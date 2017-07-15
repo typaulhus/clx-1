ec176c496c5a8b8cc94b44b63a1aa6bfc8452426
bettyfinn007 <chelseapaulhus@gmail.com>
2017-07-15 12:09:56 -0400

    Submit = true all values accepted

--- templates/process_old.php
+++ templates/process_old.php
@@ -3,28 +3,24 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
    
-header("Content-Type: application/json; charset=UTF-8");
-$postData = ($_GET);
-
-//     echo json_encode( $_POST );
-  $response = array( 'success' => false );
-//     $_POST = file_get_contents( 'php://input' );
-//     $_POST = json_decode( $_POST );
+$_POST = $_POST;
+    echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+    echo json_encode( $_POST );
+    $response = array( 'success' => false );
+    $_POST = file_get_contents( 'php://input' );
+    $_POST = json_decode( $_POST );
     // if ( $_POST['submit && empty( $_POST['honeypot ) ) {
-        $contactName = $postData[contactName];
+        $contactName = $_POST['contactName;'];
+        $email = $_POST['email;'];
+        $phone = $_POST['phone;'];
+        $company = $_POST['company;'];
+        $message = $_POST['message;'];
 
-        $email = $postData[email];
-
-        $phone = $postData[phone];
- 
-        $company = $postData[company];
-
-        $message = $postData[message];
-     
-
-         if ( $contactName != '' && $email != '' && $message != '' ) {
+        // if ( $contactName != '' && $email != '' && $message != '' ) {
            
-                var_dump($postData);
+                var_dump($_POST);
                echo json_encode( '$_POST ');
             $mailTo = 'chelseapaulhus@gmail.com';
             $subject = 'New contact form submission';
@@ -39,7 +35,7 @@ $postData = ($_GET);
             if ( $success ) {
                 $response[ 'success' ] = true;
             }
-         }
+        // }
     // }
 
     echo json_encode( $response );
